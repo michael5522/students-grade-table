c200e1588d0e6dfc1e395fab725f3da22b3b933d
michael5522 <michaelchang.kuo@gmail.com>
2020-11-21 19:46:30 +0000

    rendering grade 1-6

--- components/app.js
+++ components/app.js
@@ -1,5 +1,6 @@
 class App {
-  constructor(){
+  constructor(gradeTable){
+    this.gradeTable = gradeTable;
     this.handleGetGradesError = this.handleGetGradesError.bind(this);
     this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
   }
@@ -8,7 +9,7 @@ class App {
     console.log(error);
   }
   handleGetGradesSuccess(grades){
-    console.log(grades);
+    this.gradeTable.updateGrades(grades);
   }
   getGrades(){
     var gradesURL = "https://sgt.lfzprototypes.com/api/grades/";
--- main.js
+++ main.js
@@ -1,2 +1,4 @@
-var app = new App();
+var table = document.getElementById('tbody');
+var gTable = new GradeTable(table);
+var app = new App(gTable);
 app.start();
