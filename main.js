434a4499f688fcf05f8aed10570343a36049efea
michael5522 <michaelchang.kuo@gmail.com>
2020-12-01 23:06:51 +0000

    finished the pageheader and made sure it is working

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var table = document.querySelector('table');
 var gTable = new GradeTable(table);
-var app = new App(gTable);
+var headerElement = document.querySelector('header');
+var pageHeader = new PageHeader(headerElement)
+var app = new App(gTable, pageHeader);
 app.start();
