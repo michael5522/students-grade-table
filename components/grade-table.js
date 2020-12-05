487dec48d8b7e0a2f8e658810a558c6451adb297
michael5522 <michaelchang.kuo@gmail.com>
2020-12-05 08:13:33 +0000

    added new method to gradetable for rendering

--- components/grade-table.js
+++ components/grade-table.js
@@ -28,4 +28,43 @@ class GradeTable {
     }
   }
 
+  onDeleteClick(deleteGrade){
+    this.deleteGrade = deleteGrade;
+  }
+
+  renderGradeRow(data, deleteGrade){
+    var $row = $("<tr>");
+    var $name = $("<td>", { text: data.name });
+    var $course = $("<td>", { text: data.course });
+    var $grade = $("<td>", { text: data.grade });
+    var $delete = $("<td>");
+    var $delBtn = $("<button>", { class: "btn btn-danger", text: "DELETE", click: deleteGrade(data.id) });
+    $delete.append($delBtn);
+    $row.append($name, $course, $grade, $delete);
+    return $row;
+
+    // var row = document.createElement('tr');
+    // var name = document.createElement('td');
+    // name.textContent = data.name;
+    // row.appendChild(name);
+
+    // var course = document.createElement('td');
+    // course.textContent = data.course;
+    // row.appendChild(course);
+
+    // var grade = document.createElement('td');
+    // grade.textContent = data.grade;
+    // row.appendChild(grade);
+
+    // var deleteD = document.createElement('td');
+    // row.appendChild(deleteD);
+
+    // var deleteBtn = document.createElement('button');
+    // deleteBtn.className = "btn btn-danger";
+    // deleteBtn.textContent = "DELETE";
+    // deleteBtn.addEventListener("click", deleteGrade(data.id));
+
+    // return row;
+  }
+
 }
